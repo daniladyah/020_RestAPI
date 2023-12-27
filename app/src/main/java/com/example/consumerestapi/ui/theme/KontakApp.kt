87b4c81633f84031a5f7cbdba2c3932cdbba5fc3
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-28 02:43:14 +0700

    menambahkan Fungsi atau kelas pada CenterAlignedTopAppBar

--- app/src/main/java/com/example/consumerestapi/ui/theme/KontakApp.kt
+++ app/src/main/java/com/example/consumerestapi/ui/theme/KontakApp.kt
@@ -1,7 +1,9 @@
 package com.example.consumerestapi.ui.theme
 
 import android.icu.text.CaseMap.Title
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,5 +17,12 @@ fun TopAppBarKontak(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ){
+    CenterAlignedTopAppBar(title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
 
+            }
+        })
 }
