e380fbdaac6c7035530027f979c75ebd82aeb42e
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-28 18:56:21 +0700

    menambahkan class InsertViewModel

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.consumerestapi.ui.home.screen
 
+import androidx.lifecycle.ViewModel
+import com.example.consumerestapi.repository.KontakRepository
+
+class InsertViewModel (private  val kontakRepository: KontakRepository) : ViewModel (){
+
+    var
+
+}
