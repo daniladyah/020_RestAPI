480d18a6a753d470665a46a2eb4c138cb6d263f9
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-28 19:15:28 +0700

    menambahkan fungsi Form input siswa

--- app/src/main/java/com/example/consumerestapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/PenyediaViewModel.kt
@@ -3,12 +3,16 @@ package com.example.consumerestapi.ui
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumerestapi.ui.home.screen.InsertViewModel
 import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
     }
 }
--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -1,2 +1,17 @@
 package com.example.consumerestapi.ui.home.screen
 
+import android.widget.NumberPicker.OnValueChangeListener
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit ={},
+    enabled: Boolean = true
+){
+
+}
