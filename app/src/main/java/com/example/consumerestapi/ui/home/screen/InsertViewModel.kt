891c6de9cf7c7d06a0fd7db4ea8dcd0ca14b629d
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-28 19:00:24 +0700

    menambahkan insert contact dan update

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.consumerestapi.ui.home.screen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.consumerestapi.repository.KontakRepository
 
-class InsertViewModel (private  val kontakRepository: KontakRepository) : ViewModel (){
+class InsertViewModel (private  val kontakRepository: KontakRepository) : ViewModel () {
 
-    var
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
 }
+    suspend fun insertKontak(){
+
+
+    }    }
