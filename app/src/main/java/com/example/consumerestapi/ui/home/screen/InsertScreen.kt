c12ed7ca737e275cfbb4581ec60a065b007a39ff
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-28 19:35:43 +0700

    menambahkan fungsi EntryKontakBody

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -1,9 +1,19 @@
 package com.example.consumerestapi.ui.home.screen
 
 import android.widget.NumberPicker.OnValueChangeListener
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -12,6 +22,55 @@ fun FormInputSiswa(
     modifier: Modifier = Modifier,
     onValueChange: (InsertUiEvent) -> Unit ={},
     enabled: Boolean = true
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it )) },
+            Label = { Text("Nama" ) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.email,
+            onValueChange = {onValueChange(insertUiEvent.copy(email = it )) },
+            Label = { Text("Email" ) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nohp,
+            onValueChange = {onValueChange(insertUiEvent.copy(nohp = it )) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            Label = { Text(text = "No HP" ) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled){
+            Text(
+                text = "Isi semua data",
+                modifier = Modifier.padding(start =12.dp )
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
 ){
 
 }
