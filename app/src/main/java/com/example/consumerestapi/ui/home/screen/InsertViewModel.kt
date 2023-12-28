343931dd5b1023a967b65e9a907d3715f5185bb5
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-28 19:04:20 +0700

    menambahkan data class InsertUiState

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumerestapi.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private  val kontakRepository: KontakRepository) : ViewModel () {
 
@@ -15,6 +17,18 @@ class InsertViewModel (private  val kontakRepository: KontakRepository) : ViewMo
         insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
 }
     suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
+data class InsertUiState(
+    val  insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
 
-    }    }
+data class
